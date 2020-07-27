5d11d576bfcd096ba4fad589937104471d0543c0
Chana Kupetz <chanakupetz@gmail.com>
2020-07-27 01:56:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,14 +4,11 @@ def my_hash_creator(key, value)
   }
 end
 
-def read_from_hash(hash_to_read, favorite_book)
-  hash_to_read = {
-    :favorite_book => "frog and toad"
-  }
+def read_from_hash(hash, key)
+  hash[key]
+
   # return the correct value using the hash and key parameters
 end
-read_from_hash(hash_to_read, :favorite_book)
-
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
